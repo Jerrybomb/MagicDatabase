282dbcf5936133a31ade7ec1bc54c12b48734955
Jerrybomb <ostrowskiian@yahoo.ca>
2013-05-16 10:14:19 -0600

    Added interface

---
+++ src/MagicInterface.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*
+ Ian Ostrowski
+ Computer Science 20
+ 2012-2013 Semester two
+ Centennial High School
+ */
+public class MagicInterface 
+{
+	public static Scanner kbReader;
+	public static void main (String[]args)
+	{
+		System.out.println("");
+		
+		
+	}
+
+}
