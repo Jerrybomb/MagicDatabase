c23809219ae63559d625953597bc6ae4827e2844
chikapow <little-drummerboy@live.com>
2013-05-22 12:02:08 -0600

    removed flavor text

--- src/Card.java
+++ src/Card.java
@@ -3,17 +3,15 @@ public class Card {
 
 		
 	private String Name="";
-	private String FlavorText="";
 	private String Ability="";
 	
 	private int AbilityManaCost;
 	private int ManaCost;
 	
-	public Card(String name, String flavorText, String ability,
+	public Card(String name, String ability,
 			int abilityManaCost, int manaCost) {
 		super();
 		Name = name;
-		FlavorText = flavorText;
 		Ability = ability;
 		AbilityManaCost = abilityManaCost;
 		ManaCost = manaCost;
@@ -25,14 +23,6 @@ public class Card {
 
 	public void setName(String name) {
 		Name = name;
-	}
-
-	public String getFlavorText() {
-		return FlavorText;
-	}
-
-	public void setFlavorText(String flavorText) {
-		FlavorText = flavorText;
 	}
 
 	public String getAbility() {
